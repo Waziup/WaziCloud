5f42a7d8774114a868ab7b977a401b4f6b4b5da5
Ousmane Samba <ousmanesamba@gmail.com>
2016-09-26 20:52:55 +0000

    update waziup , subcommand + arguments + options

--- cli/waziup-cli/waziup/commands/cmd_apps.py
+++ cli/waziup-cli/waziup/commands/cmd_apps.py
@@ -1,9 +1,41 @@
 import click
 from waziup.cli import pass_context
 
-
-@click.command('apps', short_help='Waziup apps management command')
-@pass_context
+@click.group()
+@click.pass_context
 def cli(ctx):
     """Waziup apps management command"""
-    ctx.log('Waziup status debug')
+    pass
+
+@cli.command('create')
+@click.argument('<type>')
+@click.argument('<num>',help='the number of processes.')
+@click.option('--app', '-a',help='a buildpack url to use for this app')
+@click.pass_obj
+def cli_create(ctx):
+    """Waziup apps creation command"""
+    pass
+
+@cli.command('list')
+@click.pass_obj
+def cli_list(ctx):
+    """Waziup apps list apps command"""
+    pass
+
+@cli.command('deploy')
+@click.pass_obj
+def cli_deploy(ctx):
+    """Waziup apps deploy app command"""
+    pass
+
+@cli.command('scale')
+@click.pass_obj
+def cli_scale(ctx):
+    """Waziup apps scale app command"""
+    pass
+
+@cli.command('destroy')
+@click.pass_obj
+def cli_destroy(ctx):
+    """Waziup apps destroy app command"""
+    pass
--- cli/waziup-cli/waziup/commands/cmd_gateways.py
+++ cli/waziup-cli/waziup/commands/cmd_gateways.py
@@ -2,8 +2,36 @@ import click
 from waziup.cli import pass_context
 
 
-@click.command('apps', short_help='Waziup gateway management command')
-@pass_context
+@click.group()
+@click.pass_context
 def cli(ctx):
-    """Waziup gateway management command"""
-    ctx.log('Waziup status debug')
+    """Waziup gateway command"""
+    pass
+
+@cli.command('add')
+@click.pass_obj
+@click.argument('<gatewayIP>')
+def cli_add(ctx):
+    """Waziup gateway add command"""
+    pass
+
+@cli.command('remove')
+@click.argument('<gatewayIP>')
+@click.pass_obj
+def cli_remove(ctx):
+    """Waziup gateway remove command"""
+    pass
+
+@cli.command('list')
+@click.pass_obj
+def cli_list(ctx):
+    """Waziup gateway list command"""
+    pass
+
+@cli.command('sensors')
+@click.argument('<gatewayIP>')
+@click.pass_obj
+def cli_sensors(ctx):
+    """Waziup gateway sensors command"""
+    pass
+
