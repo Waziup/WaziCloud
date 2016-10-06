9d3866964282f236a9bd00f388b20dd413bb4537
Corentin Dupont <corentin.dupont@gmail.com>
2016-10-06 17:51:50 +0200

    moved function to utils

--- controller/waziupctrl/controllers/default_controller.py
+++ controller/waziupctrl/controllers/default_controller.py
@@ -6,6 +6,7 @@ from waziup_commons.models.measure_metadata import MeasureMetadata
 from subprocess import call
 import requests
 import json
+import controllers.utils
 import config as CONFIG
 
 def gateways_get() -> str:
@@ -26,76 +27,69 @@ def gateways_id_delete(id) -> str:
 def gateways_id_devices_get(id) -> str:
     return 'do some magic!'
 
-
 def data_observations_get() -> str:
 
     r = requests.get(CONFIG.conf.brokerurl + '/v2/entities')
     resp = []
     for entity in r.json():
 
-        resp.append(get_observation(entity))
+        resp.append(controllers.utils.get_observation(entity))
 
-    return str(resp)
-
-def get_observation(entity: dict) -> Observation:
-    measure = entity['measure']
-    value            = measure['value']
-    measure_metadata = measure['metadata']
-    dimension = measure_metadata['dimension']['value']
-    unit      = measure_metadata['unit']['value']
-    timestamp = measure_metadata['timestamp']['value']
-
-    my_measure = Measure(value = value,
-                         metadata = MeasureMetadata(dimension = dimension,
-                                                    unit = unit,
-                                                    timestamp = timestamp))
-    my_obs = Observation(id = entity['id'],
-                         type = entity['type'],
-                         measure = measure)
-    return my_obs
-
+    return str(list(filter(None, resp)))
 
 def data_observations_post(body) -> str:
 
     print(body)
-    id = body['id']
-    type = body['type']
-    measure_value = body.get('measure').get('value')
+    try:
+        id = body['id']
+        type = body['type']
+        measure_value = body.get('measure').get('value')
 
-    #first check if the entity exists
-    r = requests.get(CONFIG.conf.brokerurl + '/v2/entities/' + id )
-    if(r.status_code == requests.codes.ok):
+        #first check if the entity exists
+        r = requests.get(CONFIG.conf.brokerurl + '/v2/entities/' + id )
+        if(r.status_code == requests.codes.ok):
 
-        #update the entity
-        r = requests.put(CONFIG.conf.brokerurl + '/v2/entities/' + body.get('id') + '/attrs/measure/', json = {'value': str(measure_value)})
-    else:
+            #update the entity
+            r = requests.put(CONFIG.conf.brokerurl + '/v2/entities/' + body.get('id') + '/attrs/measure/', json = {'value': str(measure_value)})
+        else:
 
-        #create a new entity
-        measure = dict()
-        measure['value'] = body.get('measure').get('value')
-        measure['type'] = 'Number'
-        measure['metadata'] = dict()
-        measure['metadata']['dimension'] = dict()
-        measure['metadata']['dimension']['value'] = body['measure']['metadata']['dimension']
-        measure['metadata']['unit'] = dict()
-        measure['metadata']['unit']['value'] = body['measure']['metadata']['unit']
-        measure['metadata']['timestamp'] = dict()
-        measure['metadata']['timestamp']['value'] = body['measure']['metadata']['timestamp']
-        data = dict()
-        data['id'] = body['id']
-        data['type'] = body['type']
-        data['measure'] = measure
-        print(data)
-        r = requests.post(CONFIG.conf.brokerurl + '/v2/entities', json=data)
-        print(r.text)
+            #create a new entity
+            measure = dict()
+            measure['value'] = body.get('measure').get('value')
+            measure['type'] = 'Number'
+            measure['metadata'] = dict()
+            measure['metadata']['dimension'] = dict()
+            measure['metadata']['dimension']['value'] = body['measure']['metadata']['dimension']
+            measure['metadata']['unit'] = dict()
+            measure['metadata']['unit']['value'] = body['measure']['metadata']['unit']
+            measure['metadata']['timestamp'] = dict()
+            measure['metadata']['timestamp']['value'] = body['measure']['metadata']['timestamp']
+            data = dict()
+            data['id'] = body['id']
+            data['type'] = body['type']
+            data['measure'] = measure
+            print(data)
+            r = requests.post(CONFIG.conf.brokerurl + '/v2/entities', json=data)
+            print(r.text)
 
-    return r.text
+        return r.text
+    except KeyError:
+        return 'Invalid body', 400
 
 def data_observations_id_get(id) -> str:
 
     r = requests.get(CONFIG.conf.brokerurl + '/v2/entities/' + id)
+    if(r.status_code == requests.codes.ok):
 
-    return str(get_observation(r.json()))
+        try:
+            return str(controllers.utils.get_observation(r.json()))
+
+        except KeyError:
+            return 'Invalid body', 400
+
+    else:
+        return 'Invalid id', 404
+
 
 def data_subscriptions_get() -> str:
     return 'do some magic!'
