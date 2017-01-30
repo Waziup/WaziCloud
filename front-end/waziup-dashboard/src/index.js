00252f3ea8305654e140c3705798ffcfcb569fb9
Ousmane Samba <ousmanesamba@gmail.com>
2017-01-30 14:24:27 +0300

    docker for waziup dashboard

--- front-end/waziup-dashboard/src/index.js
+++ front-end/waziup-dashboard/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
+import Keycloak from 'keycloak-js'
 import Layout from './components/Layout';
 import Home from './components/Home';
 import './index.css';
@@ -36,5 +37,30 @@ const routes = {
 }
 
 
-ReactDOM.render(<Router history={hashHistory} routes={routes} />, document.getElementById('root'))
+// let KeycloakConfig = {
+    // "realm": "waziup",
+    // "url": "http://aam.waziup.io/auth",
+    // "resource": "mobile-app-cli",
+    // "public-client": true,
+    // "clientId":"web-waziup-cli",
+    // "credentials": {
+          // "secret": "28226338-eb58-45f5-bdae-921091a8b35c"
+        // },
+    // "policy-enforcer": {}
+// };
 
+
+// let kc = Keycloak(KeycloakConfig);
+  // console.log(kc);
+// kc.init({onLoad: 'check-sso'}).success(authenticated => {
+  // if (authenticated) {
+    //store.getState().keycloak = kc;
+    // setInterval(() => {
+      // kc.updateToken(10).error(() => kc.logout());
+    // }, 10000);
+    ReactDOM.render(<Router history={hashHistory} routes={routes} />, document.getElementById('root'))
+  // } else {
+    // show possibly other page here...
+    // kc.login();
+  // }
+// });
