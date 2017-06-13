cdb02e3d3bf11c7e015ecce6ca5f7eb4d858ae29
Corentin Dupont <corentin.dupont@gmail.com>
2017-06-13 18:18:30 +0200

    move files

--- identity/keycloak/testToken/introspect.sh
+++
@@ -1,2 +0,0 @@
-
-curl -k -u waziup:waziup -H 'Content-Type: application/x-www-form-urlencoded' -X POST --data 'token=$TOKEN' http://aam.waziup.io/auth/realms/waziup/protocol/openid-connect/token/introspect
