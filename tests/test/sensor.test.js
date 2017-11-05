323c72f96ac09eec0fc51d2864cf2fde2a7c4bf8
Henok Teklu <henokzion@gmail.com>
2017-11-05 17:34:08 +0300

    tests added for updating the sensor kind field

--- tests/test/sensor.test.js
+++ tests/test/sensor.test.js
@@ -6,6 +6,7 @@ let sensor = require('../config/sensor');
 
 chai.use(chaiHttp);
 
+// if the sensor exists delete it
 chai.request(server)
 	.delete('/domains/cdupont/sensors/0d710b12-27e8-433d-ab3a-e05b7127eeaa')
 	.end((err, rss)=>{
@@ -14,7 +15,7 @@ chai.request(server)
 
 describe('Sensors ', () => {
 
-	describe('/GET Sensors', () => {
+	describe('/domains/{domain}/sensors/{sensor_id} Get Sensors', () => {
 		it('it should GET all the the senseors', (done) => {
 			chai.request(server)
 				.get('/domains/cdupont/sensors')
@@ -25,7 +26,7 @@ describe('Sensors ', () => {
 				});
 		});
 	});
-	describe('/Post sensors', ()=>{
+	describe('/domains/{domain}/sensors/{sensor_id} Create sensors', ()=>{
 		it('it should POST a sensor ', (done) => {
 			chai.request(server)
 				.post('/domains/cdupont/sensors')
@@ -37,7 +38,7 @@ describe('Sensors ', () => {
 		  });
 	});
 
-	describe('/GET/:id sensor', () => {
+	describe('/domains/{domain}/sensors/{sensor_id} sensor', () => {
 		it('it should GET a sensor by the given id', (done) => {
 		
 			chai.request(server)
@@ -81,6 +82,74 @@ describe('Sensors ', () => {
 				});
 
 		});
-});
+	});
+	describe('/domains/{domain}/sensors/{sensor_id}/name	insert name', () => {
+		it('it should update the name field', (done) => {
+			chai.request(server)
+				.put(`/domains/cdupont/sensors/${sensor.id}/name`)
+				.set('content-type', 'text/plain')
+				.send("SEN1")
+				.end((err, res) => {
+					res.should.have.status(200);
+					chai.request(server)
+						.get(`/domains/cdupont/sensors/${sensor.id}`)
+						.end((err, res)=>{
+							res.body.should.be.a('object');
+							res.body.should.have.property('name').eql('SEN1');
+							done();
+						})
+						
+				});
+
+		});
+	
+	
+	});
+	describe('/domains/{domain}/sensors/{sensor_id}/location	insert location', () => {
+		it('it should update the location field', (done) => {
+			chai.request(server)
+				.put(`/domains/cdupont/sensors/${sensor.id}/location`)
+				.send({
+					"latitude": 5.36,
+					"longitude": 4.0083
+				  })
+				.end((err, res) => {
+					res.should.have.status(200);
+					chai.request(server)
+						.get(`/domains/cdupont/sensors/${sensor.id}`)
+						.end((err, res)=>{
+							res.body.should.be.a('object');
+							res.body.location.should.have.property('latitude').eql(5.36);
+							res.body.location.should.have.property('longitude').eql(4.0083);
+							done();
+						})
+						
+				});
+
+		});	
+	});
+
+	describe('/domains/{domain}/sensors/{sensor_id}/sensor_kind	insert sensor kind', () => {
+		it('it should update the sensor kind field', (done) => {
+			chai.request(server)
+				.put(`/domains/cdupont/sensors/${sensor.id}/sensor_kind`)
+				.set('content-type', 'text/plain')
+				.send("Soil moisture sensor")
+				.end((err, res) => {
+					res.should.have.status(200);
+					chai.request(server)
+						.get(`/domains/cdupont/sensors/${sensor.id}`)
+						.end((err, res)=>{
+							res.body.should.be.a('object');
+							res.body.should.have.property('sensor_kind').eql('Soil moisture sensor');
+							done();
+						})
+						
+				});
+
+		});
+	
+	
+	});
 
 });
