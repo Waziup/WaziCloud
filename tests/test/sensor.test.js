1ad20d685e752cfbb8d0def8b85ecb54aa684be5
Henok Teklu <henokzion@gmail.com>
2017-11-05 14:58:01 +0300

    sensors test

--- tests/test/sensor.test.js
+++ tests/test/sensor.test.js
@@ -2,6 +2,7 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let should = chai.should();
 let server = "http://localhost/api/v1";
+let sensor = require('../config/sensor');
 
 chai.use(chaiHttp);
 
@@ -26,31 +27,6 @@ describe('Sensors ', () => {
 	});
 	describe('/Post sensors', ()=>{
 		it('it should POST a sensor ', (done) => {
-			let sensor = {
-				"id": "0d710b12-27e8-433d-ab3a-e05b7127eeaa",
-				"gateway_id": "0d710b12-27e8-433d-ab3a-e05b7127eeaa",
-				"name": "Sensor1",
-				"owner": "cdupont",
-				"sensor_kind": "Soil temperature sensor",
-				"measurements": [
-				  {
-					"id": "0d710b12-27e8-433d-ab3a-e05b7127eeaa",
-					"name": "TC1",
-					"dimension": "temperature",
-					"unit": "Degree C",
-					"values": [
-					  {
-						"value": "25.6",
-						"timestamp": "2016-06-08T18:20:27.873Z"
-					  }
-					]
-				  }
-				],
-				"location": {
-				  "latitude": 5.36,
-				  "longitude": 4.0083
-				}
-			};
 			chai.request(server)
 				.post('/domains/cdupont/sensors')
 				.send(sensor)
@@ -71,16 +47,15 @@ describe('Sensors ', () => {
 						console.log(err);
 						return;
 					}
-					console.log(res);
 					res.should.have.status(200);
-					// res.body.should.be.a('object');
-					// res.body.should.have.property('gateway_id');
-					// res.body.should.have.property('name');
-					// res.body.should.have.property('owner');
-					// res.body.should.have.property('sensor_kind');
-					// res.body.should.have.property('measurements');
-					// res.body.should.have.property('location');
-					// res.body.should.have.property('id').eql("0d710b12-27e8-433d-ab3a-e05b7127eeaa");
+					res.body.should.be.a('object');
+					res.body.should.have.property('gateway_id');
+					res.body.should.have.property('name');
+					res.body.should.have.property('owner');
+					res.body.should.have.property('sensor_kind');
+					res.body.should.have.property('measurements');
+					res.body.should.have.property('location');
+					res.body.should.have.property('id').eql(sensor.id);
 					done();
 				});
 
