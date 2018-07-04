daa83dcfe43d4b9869292b8895aec4493d73f7e4
Corentin Dupont <corentin.dupont@gmail.com>
2018-07-04 18:19:26 +0200

    better style

--- tests/test/sensor.admin.test.js
+++ tests/test/sensor.admin.test.js
@@ -5,252 +5,92 @@ let baseUrl = require('../config/enviroment').baseUrl;
 let domain = require('../config/enviroment').domain;
 let sensor = require('../config/sample-data').valid;
 let invalidSensor = require('../config/sample-data').invalid;
-let userCredentials = require('../config/sample-data').user.admin;
-let token = "";
+let utils = require('./utils');
+let token = utils.getToken();
 
 chai.use(chaiHttp);
+chai.Assertion.includeStack = true;
 
 describe('Sensors with admin previledges', () => {
-  before(function (done) {
-    chai.request(baseUrl)
-      .post('/auth/token')
-      .send(userCredentials)
-      .end(function (err, response) {
-        token = response.text;
-        chai.request(baseUrl)
-          .delete(`/domains/${domain}/sensors/${sensor.id}`)
-          .set('Authorization', `Bearer ${token}`)
-          .end((err, rss) => {
-            done();
-          })
-      });
+  before(async function () {
+    try {
+      await utils.deleteSensor()
+    } catch (err) {
+      console.log('error:' + err)
+    }
   });
+
   describe('Get Sensors', () => {
-    it('sensors are returned as an array', (done) => {
-      chai.request(baseUrl)
-        .get(`/domains/${domain}/sensors`)
-        .set('Authorization', `Bearer ${token}`)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('array');
-          done();
-        });
+    it('sensors are returned as an array', async () => {
+      let res = await chai.request(baseUrl).get(`/domains/${domain}/sensors`).set('authorization', `Bearer ${token}`)
+      res.should.have.status(200);
+      res.body.should.be.a('array');
     });
   });
   describe('Create sensors', () => {
-    it('sensor is created', (done) => {
-      chai.request(baseUrl)
-        .post(`/domains/${domain}/sensors`)
-        .set('authorization', `Bearer ${token}`)
-        .send(sensor)
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
+    it('sensor is created', async () => {
+      let res = await chai.request(baseUrl).post(`/domains/${domain}/sensors`).set('authorization', `Bearer ${token}`).send(sensor)
+      res.should.have.status(200);
     });
-    it('sensor with the same id is rejected', (done) => {
-      chai.request(baseUrl)
-        .post(`/domains/${domain}/sensors`)
-        .set('Authorization', `Bearer ${token}`)
-        .send(sensor)
-        .end((err, res) => {
-          res.should.have.status(422);
-          done();
-        });
+    it('sensor with the same id is rejected', async () => {
+      let res = await chai.request(baseUrl).post(`/domains/${domain}/sensors`).set('Authorization', `Bearer ${token}`).send(sensor)
+      res.should.have.status(422);
     });
-    it('sensor with invalid data is rejected', (done) => {
-      chai.request(baseUrl)
-        .post(`/domains/${domain}/sensors`)
-        .set('Authorization', `Bearer ${token}`)
-        .send(invalidSensor)
-        .end((err, res) => {
-          res.should.have.status(400);
-          done();
-        });
+    it('sensor with invalid data is rejected', async () => {
+      let res = await chai.request(baseUrl).post(`/domains/${domain}/sensors`).set('Authorization', `Bearer ${token}`).send(invalidSensor)
+      res.should.have.status(400);
     });
   });
 
   describe('Get a Single Sensor', () => {
-    it('retrieved sensor has all the correct values', (done) => {
-      chai.request(baseUrl)
-        .get(`/domains/${domain}/sensors/${sensor.id}`)
-        .set('Authorization', `Bearer ${token}`)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          //all fields of original sensor should be here
-          res.body.should.deep.include(sensor);
-          //read-only fields should be here
-          res.body.should.have.property('date_created');
-          res.body.should.have.property('date_modified');
-          done();
-        });
+    it('retrieved sensor has all the correct values', async () => {
+      let res = await chai.request(baseUrl).get(`/domains/${domain}/sensors/${sensor.id}`).set('Authorization', `Bearer ${token}`)
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      //all fields of original sensor should be here
+      res.body.should.deep.include(sensor);
+      //read-only fields should be here
+      res.body.should.have.property('date_created');
+      res.body.should.have.property('date_modified');
     });
-    it('non existent id is rejected', (done) => {
-      chai.request(baseUrl)
-        .get(`/domains/${domain}/sensors/this-id-does-not-exist`)
-        .set('Authorization', `Bearer ${token}`)
-        .end((err, res) => {
-          res.should.have.status(404);
-          done();
-        });
-
+    it('non existent id is rejected', async () => {
+      let res = await chai.request(baseUrl).get(`/domains/${domain}/sensors/this-id-does-not-exist`).set('Authorization', `Bearer ${token}`)
+      res.should.have.status(404);
     });
   });
 
   describe('Insert Owner', () => {
-    it('owner field should be updated', (done) => {
-      chai.request(baseUrl)
-        .put(`/domains/${domain}/sensors/${sensor.id}/owner`)
-        .set('Authorization', `Bearer ${token}`)
-        .set('content-type', 'text/plain')
-        .send("henok")
-        .end((err, res) => {
-          res.should.have.status(200);
-          chai.request(baseUrl)
-            .get(`/domains/${domain}/sensors/${sensor.id}`)
-            .end((err, res) => {
-              res.body.should.be.a('object');
-              res.body.should.have.property('owner').eql('henok');
-              done();
-            })
-
-        });
-
+    it('owner field should be updated', async () => {
+      let res = await chai.request(baseUrl).put(`/domains/${domain}/sensors/${sensor.id}/owner`).set('Authorization', `Bearer ${token}`).set('content-type', 'text/plain').send("henok")
+      res.should.have.status(200);
+      let res2 = await chai.request(baseUrl).get(`/domains/${domain}/sensors/${sensor.id}`)
+      res2.body.should.have.property('owner').eql('henok');
     });
   });
   describe('Insert Name', () => {
-    it('name field should be updated', (done) => {
-      chai.request(baseUrl)
-        .put(`/domains/${domain}/sensors/${sensor.id}/name`)
-        .set('Authorization', `Bearer ${token}`)
-        .set('content-type', 'text/plain')
-        .send("SEN1")
-        .end((err, res) => {
-          res.should.have.status(200);
-          chai.request(baseUrl)
-            .get(`/domains/${domain}/sensors/${sensor.id}`)
-            .end((err, res) => {
-              res.body.should.be.a('object');
-              res.body.should.have.property('name').eql('SEN1');
-              done();
-            })
-        });
+    it('name field should be updated', async () => {
+      let res = await chai.request(baseUrl).put(`/domains/${domain}/sensors/${sensor.id}/name`).set('Authorization', `Bearer ${token}`).set('content-type', 'text/plain').send("SEN1")
+      res.should.have.status(200);
+      let res2 = chai.request(baseUrl).get(`/domains/${domain}/sensors/${sensor.id}`)
+      res2.body.should.be.a('object');
+      res2.body.should.have.property('name').eql('SEN1');
     });
   });
   describe('Insert Location', () => {
-    it('Location field should be updated', (done) => {
-      chai.request(baseUrl)
-        .put(`/domains/${domain}/sensors/${sensor.id}/location`)
-        .set('Authorization', `Bearer ${token}`)
-        .send({
-          "latitude": 5.36,
-          "longitude": 4.0083
-        })
-        .end((err, res) => {
-          res.should.have.status(200);
-          chai.request(baseUrl)
-            .get(`/domains/${domain}/sensors/${sensor.id}`)
-            .end((err, res) => {
-              res.body.should.be.a('object');
-              res.body.location.should.have.property('latitude').eql(5.36);
-              res.body.location.should.have.property('longitude').eql(4.0083);
-              done();
-            })
-
-        });
-
-    });
-  });
-  
-  describe('Get Measurements', () => {
-    it('measurements are returned as an array', (done) => {
-      chai.request(baseUrl)
-        .get(`/domains/${domain}/sensors/${sensor.id}/measurements`)
-        .set('Authorization', `Bearer ${token}`)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('array');
-          done();
-        });
-    });
-  });
-  
-  describe('Create measurement', () => {
-    it('measurement is created', (done) => {
-      chai.request(baseUrl)
-        .post(`/domains/${domain}/sensors/${sensor.id}/measurements`)
-        .set('authorization', `Bearer ${token}`)
-        .send(sensor.measurements[0])
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
-  });
-  describe('Get a measurement', () => {
-    it('retrieved measurement has all the correct values', (done) => {
-      chai.request(baseUrl)
-        .get(`/domains/${domain}/sensors/${sensor.id}/measurements/${sensor.measurements[0].id}`)
-        .set('Authorization', `Bearer ${token}`)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          //all fields of original sensor should be here
-          res.body.should.deep.include(sensor.measurements[0]);
-          done();
-        });
-    });
-  });
-  describe('Create measurement value', () => {
-    it('measurement value is created', (done) => {
-      chai.request(baseUrl)
-        .post(`/domains/${domain}/sensors/${sensor.id}/measurements/${sensor.measurements[0].id}/values`)
-        .set('authorization', `Bearer ${token}`)
-        .send({"value": "25.6", "timestamp": "2016-06-08T18:20:27.873Z"})
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
-  });
-  describe('Get measurement values', () => {
-    it('retrieved measurement values are correct', (done) => {
-      chai.request(baseUrl)
-        .get(`/domains/${domain}/sensors/${sensor.id}/measurements/${sensor.measurements[0].id}/values`)
-        .set('Authorization', `Bearer ${token}`)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('array');
-          //all fields should be here
-          res.body[0].should.include({"value": "25.6", "timestamp": "2016-06-08T18:20:27.873Z"});
-          res.body[0].should.have.property("date_received");
-          done();
-        });
-    });
-    it('measurement last value is correct', (done) => {
-      chai.request(baseUrl)
-        .get(`/domains/${domain}/sensors/${sensor.id}/measurements/${sensor.measurements[0].id}`)
-        .set('Authorization', `Bearer ${token}`)
-        .end((err, res) => {
-          res.should.have.status(200);
-          //all fields should be here
-          res.body.last_value.should.include({"value": "25.6", "timestamp": "2016-06-08T18:20:27.873Z"});
-          res.body.last_value.should.have.property("date_received");
-          done();
-        });
+    it('Location field should be updated', async () => {
+      let res = await chai.request(baseUrl).put(`/domains/${domain}/sensors/${sensor.id}/location`).set('Authorization', `Bearer ${token}`).send({"latitude": 5.36, "longitude": 4.0083})
+      res.should.have.status(200);
+      let res2 = chai.request(baseUrl).get(`/domains/${domain}/sensors/${sensor.id}`)
+      res.body.should.be.a('object');
+      res.body.location.should.have.property('latitude').eql(5.36);
+      res.body.location.should.have.property('longitude').eql(4.0083);
     });
   });
 
   describe('Remove Sensor', () => {
-    it('sensor should be removed', (done) => {
-      chai.request(baseUrl)
-        .delete(`/domains/${domain}/sensors/${sensor.id}`)
-        .set('Authorization', `Bearer ${token}`)
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
+    it('sensor should be removed', async () => {
+      let res = await chai.request(baseUrl).delete(`/domains/${domain}/sensors/${sensor.id}`).set('Authorization', `Bearer ${token}`)
+      res.should.have.status(200);
     });
   });
 
