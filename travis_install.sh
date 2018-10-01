48013f9a667b4d18c05d192dbdf63ff2eaad4de5
Corentin Dupont <corentin.dupont@gmail.com>
2018-10-01 15:26:45 +0200

    trav

--- travis_install.sh
+++ travis_install.sh
@@ -29,6 +29,10 @@ setup_dependencies() {
   "
 
   sudo apt update -y
+  sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt update -y
   sudo apt install --only-upgrade docker-ce -y
 
   sudo rm /usr/local/bin/docker-compose
