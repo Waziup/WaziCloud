65f725ca766f6f82c621fa93a3290f1f06e22bb3
Corentin Dupont <corentin.dupont@gmail.com>
2018-10-01 20:50:15 +0200

    trav

--- travis_install.sh
+++ travis_install.sh
@@ -44,7 +44,7 @@ setup_dependencies() {
   curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
   sudo apt-get install nodejs
 
-  sudo apt-get install mutt
+  sudo apt-get install mailutils
 
   docker-compose --version
   docker info
