54b561f0831e1d7989326bb2ea7ce17f9f8dfa03
Corentin Dupont <corentin.dupont@gmail.com>
2018-10-18 12:55:42 +0200

    cosmetic

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,9 @@
-
+echo "******** Starting the platform... **********"
 docker-compose up > /dev/null &
+
 attempt_counter=0
 max_attempts=40
-
+#Wait until the platform started
 until $(curl --output /dev/null --silent --head --fail  http://localhost:800/api/v1/domains/waziup/sensors); do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
       echo "Max attempts reached: platform didn't start correctly"
@@ -14,11 +15,15 @@ until $(curl --output /dev/null --silent --head --fail  http://localhost:800/api
     sleep 5
 done
 
+echo "******** Platform stated, running tests... *********"
 cd tests
 npm install
+#run tests
 npm test 2>&1 > log.txt
 exit_code=$?
+echo "******** Tests finished, shuting down the platform... *********"
 docker-compose down
+echo "******** Tests results: ********"
 cat log.txt
 exit $exit_code
 
