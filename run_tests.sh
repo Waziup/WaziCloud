f7506211047cb7e4e2ae621f56ffe878305b012d
Corentin Dupont <corentin.dupont@gmail.com>
2019-06-10 17:15:36 +0200

    docker

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 echo "******** Starting the platform... **********"
-docker-compose up > log_platform.txt &
+docker-compose -f docker-compose.yml -f docker-compose-first-run.yml up > log_platform.txt &
 
 attempt_counter=0
 max_attempts=40
