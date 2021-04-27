619bf3c8234cfdcc7c07c1ffff0cce46011ddccd
Corentin Dupont <corentin.dupont@gmail.com>
2021-04-27 16:24:44 +0200

    tests

---
+++ tests/index.js
@@ -0,0 +1,11 @@
+
+describe('WaziCloud API', function() {
+    require('./test/devices/device.test.js');
+    require('./test/devices/sensor.test.js');
+    require('./test/devices/actuator.test.js');
+    require('./test/gateways/gateways.test.js');
+    require('./test/notif/notifications.test.js');
+    require('./test/projects/projects.test.js');
+    require('./test/users/users.test.js');
+    require('./test/mqtt/mqtt.test.js');
+});
