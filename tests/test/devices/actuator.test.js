33da08b68497e19077478d585b76b5f509b811d5
OliverMengich <kemeioliver2000@gmail.com>
2025-01-10 12:50:30 +0300

    refactor: chagnes in admin auth

--- tests/test/devices/actuator.test.js
+++ tests/test/devices/actuator.test.js
@@ -167,7 +167,7 @@ describe('Actuators', () => {
     });
 
     it('normal user can remove own Actuator', async () => {
-      const resD = await deleteDevice(device.id).set(withAdmin);
+      const resD = await deleteDevice(device.id).set(withNormal);
       resD.should.have.status(204);
 
       const resC = await createDevice(device).set(withNormal);
