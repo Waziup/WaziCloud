0f71e0ed199d1b94dbd6bf717db24ef59885c392
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2025-01-17 11:47:35 +0300

    refactor: undefined checker for API_URL

--- tests/config/env.js
+++ tests/config/env.js
@@ -1,5 +1,5 @@
 module.exports ={
-  apiUrl:  process.env.API_URL  || "http://localhost:800/api/v2",
+  apiUrl:  process.env.API_URL ? process.env.API_URL+"/api/v2" : "http://localhost:800/api/v2",
   mqttUrl: process.env.MQTT_URL || "tcp://127.0.0.1:3883",
   domain: "waziup"
 }
